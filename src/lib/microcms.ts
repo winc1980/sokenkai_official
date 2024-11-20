3da2d6d29a66974ef7cc315bc677e35cac61a347
fuma <101544710+fumaSuzuki@users.noreply.github.com>
2024-11-20 18:46:03 +0900

    add blog

--- src/lib/microcms.ts
+++ src/lib/microcms.ts
@@ -38,3 +38,36 @@ export const getTopicDetail = async (
     queries,
   });
 };
+
+
+//ブログ型定義
+export type Blog = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+};
+export type BlogResponse = {
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Blog[];
+};
+
+//APIの呼び出し
+export const getBlogs = async (queries?: MicroCMSQueries) => {
+  return await client.get<BlogResponse>({ endpoint: "blogs", queries });
+};
+export const getBlogDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  return await client.getListDetail<Blog>({
+    endpoint: "blogs",
+    contentId,
+    queries,
+  });
+};
